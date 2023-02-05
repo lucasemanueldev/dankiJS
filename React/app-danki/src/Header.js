5980fd81b52efaec00885b435e6a8140155e3bde
Lucas Emanuel <88749179+lucasemanueldev@users.noreply.github.com>
2023-02-04 21:08:26 -0300

    componentes

---
+++ React/app-danki/src/Header.js
@@ -0,0 +1,12 @@
+//import './App.css';
+
+function Header() {
+  return (
+    <div>
+      <h1>eu sou o header</h1>
+      <a href="https://pbs.twimg.com/profile_images/1294819965632163840/zL35EMhv_400x400.jpg">macaco</a>
+    </div>
+  );
+}
+
+export default Header;
