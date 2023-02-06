5a4639e6acfbf9bb6497da785c9798c8cc9b301a
Lucas Emanuel <88749179+lucasemanueldev@users.noreply.github.com>
2023-02-05 21:01:57 -0300

    Fim React

--- React/previsao-do-tempo/src/Search.js
+++ React/previsao-do-tempo/src/Search.js
@@ -13,20 +13,42 @@ function Search(props){
         .then(response=> response.json())
         .then(data=>{
             const {main, name, sys, weather} = data;
-            if(sys != undefined)
-                console.log(sys);
-            if(weather != undefined)
-                console.log(weather[0]['description']);
-        })
-    }
+
+            if(sys != undefined){
+                if(weather != undefined){
+                    const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+                    setCidade(`
+                    <div class="dados">
+                    <p>Temperatura: ${main.temp}</p>
+                    <p>País: ${sys.country}</p>
+                    <p>Cidade: ${name}</p>
+                    <p>Céu: ${weather[0]['description']}</p>
+                    <img src="${icon}" />
+                    </div>
+                    `);
+                }
+                }else{
+                    setCidade("");
+                }
+            })    
+        }
 
     return(
+        <div className="searchWraper">
         <div className="search">
         <h3>Digite a cidade que você deseja saber a previsão</h3>
         <form onSubmit={(e)=>searchInput(e)}>
         <input placeholder="Digite a cidade..." type="text" name="searchInput"></input>
         <input type="submit" value="PESQUISAR" />
         </form>
+        </div>
+
+        {
+            (cidade!= "")?
+            <div dangerouslySetInnerHTML={{__html: cidade}} />:
+            <div>Pesquise por algo acima...</div>
+        }
+
         </div>
     )
 }
